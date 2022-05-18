c0b50f3019877f57a1d52fe6039c0a4d5c36ac1b
marcio-r-nunes <marcio.nunes@talkdesk.com>
2022-05-18 09:55:23 +0100

    after prettier

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-import './util/module-alias';
-import { Server } from '@overnightjs/core';
-import bodyParser from 'body-parser';
-import { ForecastController } from './controllers/forecast';
-import { Application } from 'express';
+import "./util/module-alias";
+import { Server } from "@overnightjs/core";
+import bodyParser from "body-parser";
+import { ForecastController } from "./controllers/forecast";
+import { Application } from "express";
 
 export class SetupServer extends Server {
-    constructor(private port = 3000) {
-        super();
-    }
+  constructor(private port = 3000) {
+    super();
+  }
 
-    public init(): void{
-        this.setupExpress();
-        this.setControllers();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController])
-    }
+  private setControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
--- test/index.d.ts
+++ test/index.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-var */
 
 declare global {
-    var testRequest: import('supertest').SuperTest<import('supertest').Test>;
+  var testRequest: import("supertest").SuperTest<import("supertest").Test>;
 }
 
 export {};
